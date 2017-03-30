5d225377b3d0c6f8fc9afdfbed51e4623f04e505
jacobiw <jacobiw@users.noreply.github.com>
2017-03-30 16:30:35 -0700

    Update leet15.py

--- leet15.py
+++ leet15.py
@@ -32,4 +32,44 @@ class Solution(object):
                             ans.append(newans)
                         break
         return ans
+# n^2
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        ans=[]
+        nums.sort()
+        newnums = nums
+        arrlen= len(nums)
+        
+        for i in xrange(arrlen-2):
+            if i and newnums[i] == newnums[i-1]:
+                continue
+            L,R = i+1, arrlen-1
+            target = -1* newnums[i]
+            # if newnums[L] == newnums[L+1]:
+            #     L+=1
+            # if newnums[R] == newnums[R-1]:
+            #     R-=1
+            while R>L:
+                sumrl = newnums[L] + newnums[R]
+                if sumrl == target:
+                    ans.append([newnums[i],newnums[L],newnums[R]])
+                    R-=1
+                    L+=1
+                    # if R>L:
+                    while R>L and newnums[L] == newnums[L-1]:
+                        L+=1
+                    while R>L and newnums[R] == newnums[R+1]:
+                        R-=1
+                elif sumrl > target:
+                    R-=1
+                else:
+                    L+=1
+        return ans
+        
+ 
+       
         
