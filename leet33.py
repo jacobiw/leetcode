f958fa0a94dbda82a29912e9a0c9112ae6fb1528
jacobiw <jacobiw@users.noreply.github.com>
2017-05-30 11:33:55 -0700

    Update leet33.py

--- leet33.py
+++ leet33.py
@@ -18,6 +18,32 @@ class Solution(object):
             else:
                 hi = mid
         return low if target in nums[low:low+1] else -1
+#     //////////////////////////////////////////////////////////////////////////////
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        arrlen = len(nums)
+        low, hi = 0, arrlen-1
+        if arrlen <=1:
+            if arrlen ==1 and target == nums[0]:
+                return 0
+            return -1
+        while hi >low:
+            
+            mid = (hi+low)/2
+            if target == nums[mid]: return mid
+            if (nums[0]> target) ^ (nums[0]>nums[mid]) ^ (target > nums[mid]):
+                low = mid+1
+            else:
+                hi = mid
+        return low if target == nums[low] else -1
+
+                
+                
                 
                 
                 
