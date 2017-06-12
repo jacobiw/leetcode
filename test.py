2dc4fdce29129b0753b670f7e7868df9bd7b3e46
jacobiw <jacobiw@users.noreply.github.com>
2017-06-12 05:06:36 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,27 @@
-candidates = [2, 3, 6, 7]
-target=7
+candidates = [8, 7, 4, 3]
+target=11
 def search(sumli, state, target, st):
-    if sumli >= target:
-        if sumli == target:
-            ans.append(state)
-            # print ans
-        
-        print 'return'
-        
-        
-        return
+
     for i in range(st, len(candidates)):
         # state.append(i)
+        if sumli + candidates[i] >= target:
+            print 'fist', sumli + candidates[i]
+            if sumli + candidates[i] == target:
+                ans.append(state + [candidates[i]])
+                print ans
+            
+            print state, 'and', candidates[i]
+            print 'return'
+            
+            
+            return
         
         print state
         print sumli
         raw_input()
         search(sumli+ candidates[i], state+[candidates[i]], target, i)
         # state.pop()
+        
 
 ans = []
 
