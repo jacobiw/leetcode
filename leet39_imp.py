5c760a6055e543c0f049b4e53ad5d3036f8eb9d7
jacobiw <jacobiw@users.noreply.github.com>
2017-06-13 11:54:35 -0700

    Create leet39_imp.py
    backward check
    state.append and state.pop problem. recursion will remember all state if using append
    if we choose to use i+1 instead of i in the search, it will repeatedly to find the current ele again (check leet40)

--- leet39_imp.py
+++ leet39_imp.py
@@ -24,6 +24,44 @@ class Solution(object):
         
         search(0, [], target, 0)
         return ans
+#         ^ this code will change start condition for for loop, it can avoid [2,2,3]/ [2,3,2], and [2,2,2,2],[2,2,2,3].....
+        candidates = [2, 3, 6, 7]
+        target=7
+        def search(sumli, state, target, ans):
+            if sumli >= target:
+                if sumli == target:
+                    ans.append(state)
+                    print ans
+                print 'return'
+                return
+            for i in candidates:
+                # state.append(i)
+                print state
+                print sumli
+                raw_input()
+                search(sumli+i, state+[i], target, ans)
+                # state.pop() 
+        ans = []
+        search(0, [], target, ans)
+        print ans
         
-        
-        
+#         ^ this code is the simplist dfs        
+        candidates = [2, 3, 6, 7]
+        target=7
+        def search(sumli, state, target, st):
+            if sumli >= target:
+                if sumli == target:
+                    ans.append(state)
+                    # print ans
+                return
+            for i in range(st, len(candidates)):
+                # state.append(i)
+                print state
+                print sumli
+                raw_input()
+                search(sumli+ candidates[i], state+[candidates[i]], target, i)
+                # state.pop()
+        ans = []
+        search(0, [], target, 0)
+        print ans
+ #         ^ this code will change start condition for for loop, it can avoid [2,2,3]/ [2,3,2]       
