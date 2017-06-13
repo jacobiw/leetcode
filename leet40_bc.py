32f6af58564935d7e655f4cdd706ebb9eb1d781e
jacobiw <jacobiw@users.noreply.github.com>
2017-06-13 11:56:30 -0700

    Create leet40_bc.py

---
+++ leet40_bc.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        
+        def search(sumli, state, st):
+            for i in range(st, len(candidates)):
+                if i>st and candidates[i-1] == candidates[i]: continue
+                if sumli + candidates[i] >= target:
+                    if sumli + candidates[i] == target:
+                        ans.append(state+[candidates[i]])
+                    return
+                search(sumli + candidates[i],state+[candidates[i]],i+1 )
+        ans =[]
+        candidates.sort()
+        search(0,[],0)
+        # print ans
+        return ans
