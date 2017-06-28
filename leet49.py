c31e874931b7ef3974d59312f9c3ec1fbf17eb7f
jacobiw <jacobiw@users.noreply.github.com>
2017-06-27 22:58:28 -0500

    Update leet49.py

--- leet49.py
+++ leet49.py
@@ -15,6 +15,30 @@ class Solution(object):
                 cluster+=1
             ans[dic[ele]].append(i)
         return ans
+# ^^^^time limit exceed 
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        dic = {}
+        cluster =0
+        ans=[]
+        dict = {}
+        for i in strs:
+            ele = ''.join(sorted(i))
+            dic[ele] = [i] if ele not in dic else dic[ele]+[i]
+            # if ele not in dic.keys():
+            #     dic[ele] = [i]
+            # else:
+            #     dic[ele]+=[i]
+        for key in dic:
+            ans+=[dic[key]]
+        return ans
+
+
+                
 
                 
                 
